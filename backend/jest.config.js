645f3cf5de7e05980e324c828067a9dffe4de01d
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-03-02 13:21:45 -0300

    feat: few adjusts

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -52,7 +52,7 @@ module.exports = {
     // forceCoverageMatch: [],
 
     // A path to a module which exports an async function that is triggered once before all test suites
-    globalSetup: './sap-hana.config.js',
+    // globalSetup: '',
 
     // A path to a module which exports an async function that is triggered once after all test suites
     // globalTeardown: undefined,
