498ec5e530efdda0c5b408d0f449b9d7959d7585
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-03-09 21:45:16 -0300

    fix: removing log

--- backend/__tests__/unit/helpers/http/passport-noauth.spec.js
+++ backend/__tests__/unit/helpers/http/passport-noauth.spec.js
@@ -3,8 +3,6 @@ const { Passport } = require('../../../../src/helpers/http');
 describe('Passport helper', () => {
     describe('Success', () => {
         it('should override strategy authenticate', () => {
-            console.log(Passport.prototype.authenticate);
-
             expect(Passport.prototype).toEqual(
                 expect.objectContaining({
                     authenticate: expect.any(Function)
