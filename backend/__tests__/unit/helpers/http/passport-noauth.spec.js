3903f6211540a49c1b86e7a044d9b1d2622bddf1
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-03-09 21:46:45 -0300

    fix: indentation

--- backend/__tests__/unit/helpers/http/passport-noauth.spec.js
+++ backend/__tests__/unit/helpers/http/passport-noauth.spec.js
@@ -1,13 +1,11 @@
 const { Passport } = require('../../../../src/helpers/http');
 
 describe('Passport helper', () => {
-    describe('Success', () => {
-        it('should override strategy authenticate', () => {
-            expect(Passport.prototype).toEqual(
-                expect.objectContaining({
-                    authenticate: expect.any(Function)
-                })
-            );
-        });
+    it('should override strategy authenticate', () => {
+        expect(Passport.prototype).toEqual(
+            expect.objectContaining({
+                authenticate: expect.any(Function)
+            })
+        );
     });
 });
